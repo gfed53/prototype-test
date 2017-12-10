8e808e8366e11bc30e6d1e3228e65a21d5f32099
gfed53 <gfederico7153@gmail.com>
2017-12-10 16:54:52 -0500

    Initial commit.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(function(){
+	console.log('elo world');
+
+	var mySwiper = new Swiper('.swiper-container', {
+	    nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      mousewheelControl: false
+	});
+
+	// mySwiper = $('.swiper-container').swiper;
+
+});
