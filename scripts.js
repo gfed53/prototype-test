4ded49bd00931dac20751b26b514f7a002f72cba
gfed53 <gfederico7153@gmail.com>
2017-12-11 16:39:22 -0500

    Added buttons on all slides. Added button tap animation.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,41 @@
 $(function(){
 	console.log('elo world');
 
+	// Swiper
+
 	var mySwiper = new Swiper('.swiper-container', {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        mousewheelControl: false
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev',
+		mousewheelControl: false
 	});
 
-	// mySwiper = $('.swiper-container').swiper;
+	// Toggle Button 
+	$('.swiper-slide').on('click', '.btn-toggle', function(){
+		// console.log('this',$(this));
+		// console.log('parent?',$(this).parent().parent());
+
+		let $button = $(this);
+		let $parentGrid = $(this).parent().parent();
+
+		console.log('$button',$button);
+
+		// Button animation
+		$button.addClass('btn-toggle-pressed');
+		let timeout = setTimeout(() => {
+			$button.removeClass('btn-toggle-pressed');
+			clearTimeout(timeout);
+		}, 200);
+
+		
+
+
+		// $button.velocity({
+		// 	'box-shadow': 'none'
+		// }, {
+		// 	duration: 200
+		// });
+
+	});
+	
 
 });
